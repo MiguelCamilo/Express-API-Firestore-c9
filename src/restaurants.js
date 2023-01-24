9389f969d23a636a7083ba60a9aeb6b8c37e89de
Miguel Camilo <miguelcamilo48@gmail.com>
2023-01-24 18:27:38 -0500

    API is now able to use CRUD to mutate the database

--- src/restaurants.js
+++ src/restaurants.js
@@ -2,25 +2,39 @@ import dbConnect from "./dbConnect.js"// import db connection
 import { initializeApp, cert, getApps } from "firebase-admin/app"
 import { service_account } from "../secrets.js"
 
-const colletionName = 'restaurants'
+const colletionName = 'subway'
 
 initializeApp({
 	credential: cert(service_account),
 });
 
+
+const pizza_Hawaiian = {
+    type: "Hawaiian",
+    ingredients: "pineapple , tomato , cheese , ham",
+    addons: "black pepper , parmesan cheese",
+    size: "large",
+    price: 13.99,
+    togo: false
+};
+
+
 //! GET
 export const getAllRestaurants = async (req,res) => {
     const db = dbConnect()
     // query to get all data 
-    const collection = db.collection(colletionName).get()
-    const restaurants = (await collection).docs.map( (element) => {
-        return rest = element.data()
-    })
+    const collection = await db.collection(colletionName).get()
 
+    const restaurants =  collection.docs.map( (element) => {
+        let food = element.data()
+        food.id = element.id
+        return food
+    })
+    console.table(restaurants)
     res.send(restaurants)
 }
 
-//! GET DOC
+//! GET DOC ID
 export const getRestaurantById = async (req,res) => {
     const db = dbConnect()
     // using destructuring to pull restId
@@ -38,21 +52,20 @@ export const createResturant = async (req,res) => {
     const db = dbConnect()
 
     // req.body means it the request that is coming will have a body
-    const newRestaurant = req.body 
-    await db.collection(colletionName).add(newRestaurant)
+    await db.collection(colletionName).add(pizza_Hawaiian)
     res.status(201).send('Added Restaurant')
 }
 
 //! UPDATE
-export const updateRestaurant = async (req,res) => {
-    const { restId } = req.params
-    const updateInfo = req.body
+// export const updateRestaurant = async (req,res) => {
+//     const { restId } = req.params
+//     const updateInfo = req.body
 
-    const db = dbConnect()
+//     const db = dbConnect()
 
-    await db.collection(colletionName).doc(restId).update(updateInfo)
-    res.status(202).send('Restaurant Updated')
-}
+//     await db.collection(colletionName).doc(restId).update(updateInfo)
+//     res.status(202).send('Restaurant Updated')
+// }
 
 //! DELETE
 
